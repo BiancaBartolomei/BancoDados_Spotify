8dbe54280114c72136dcd0af2d2e13cff30c886a
biancarbartolomei <biancarbartolomei@gmail.com>
2018-09-28 22:05:20 -0300

    [ADD]Albuns com mais explicit

--- functions_views_triggers.sql
+++ functions_views_triggers.sql
@@ -19,3 +19,28 @@ create trigger atualiza_top_musicas_longas
 after insert or update or delete on spotify_db.track
 for each statement execute
 procedure spotify_db.top_musicas_mais_longas_view()
+
+------------------------------------------------------------------------------------------------------------------------
+--Top 10 albuns com maior quantidade de musicas explicitas--
+
+create or replace function spotify_db.top_albuns_explicit_view()
+returns trigger as $$
+begin
+execute
+'create or replace view spotify_db.albuns_explicit
+as select a.album_name, art.artist_name, count(t.track_explicit) as quant
+from spotify_db.album a join spotify_db.track_album ta on a.album_id = ta.album_id
+join spotify_db.track t on t.track_id = ta.track_id join spotify_db.track_artist tart on
+tart.track_id = t.track_id join spotify_db.artist art on art.artist_id = tart.artist_id
+where t.track_explicit = True
+group by (a.album_name, art.artist_name)
+order by quant desc
+limit 10';
+return new;
+end;
+$$ language plpgsql;
+
+create trigger atualiza_top_albuns_explicit
+after insert or update or delete on spotify_db.track
+for each statement execute
+procedure spotify_db.top_albuns_explicit_view()
