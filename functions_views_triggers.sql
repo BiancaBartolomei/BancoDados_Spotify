0602674f4d3718f12abfe5aa3abec47cc95355aa
Luis Malta <luiso.malta@gmail.com>
2018-09-28 22:12:36 -0300

    [ADD]Funções triggers e view do Luis

--- functions_views_triggers.sql
+++ functions_views_triggers.sql
@@ -44,3 +44,58 @@ create trigger atualiza_top_albuns_explicit
 after insert or update or delete on spotify_db.track
 for each statement execute
 procedure spotify_db.top_albuns_explicit_view()
+
+------------------------------------------------------------------------------------------------------------------------
+-- Top 10 artistas com músicas mais dancantes
+
+create or replace function spotify_db.artistas_dancantes() returns trigger as $$
+	begin
+		execute 'create or replace view spotify_db.artistas_dancantes_view as' ||
+		 ' select distinct a.artist_name, t.track_dancebility
+			from spotify_db.track t join spotify_db.track_artist q on t.track_id = q.track_id
+			join spotify_db.artist a on a.artist_id = q.artist_id
+			order by t.track_dancebility desc, a.artist_name
+			limit 10';
+		return new;
+	end;
+	$$ language plpgsql;
+
+create trigger trigger_artistas_dancantes after insert or update or delete on spotify_db.track
+for each statement execute procedure  spotify_db.artistas_dancantes();
+
+------------------------------------------------------------------------------------------------------------------------
+-- Top 10 albuns mais energerticos
+
+create or replace function spotify_db.albuns_energiticos() returns trigger as $$
+  begin
+		execute ' create or replace view spotify_db.albuns_energeticos_view as ' ||
+		 'select  distinct a.album_name, t.track_energy
+			from spotify_db.album a join spotify_db.track_album q on a.album_id = q.album_id
+			join spotify_db.track t on q.track_id = t.track_id
+			order by t.track_energy desc
+			limit 10';
+		return new;
+	end;
+$$ language plpgsql;
+
+create trigger trigger_albuns_energeticos after insert or update or delete on spotify_db.track
+for each statement execute procedure  spotify_db.albuns_energiticos();
+
+------------------------------------------------------------------------------------------------------------------------
+-- Top 10 albuns mais populares com mais de 50% acustico
+
+create or replace function spotify_db.album_acustico_popularidade() returns trigger as $$
+	begin
+		execute 'create or replace view spotify_db.album_acustico_popularidade_view as select distinct (a.album_name), t.track_popularity
+			from spotify_db.track t join spotify_db.track_album q on t.track_id = q.track_id
+			join spotify_db.album a on a.album_id = q.album_id
+			where t.track_acousticness > 0.5
+			order by t.track_popularity desc
+			limit 10';
+		return new;
+	end;
+$$ language plpgsql;
+
+
+create trigger trigger_album_acustico_popularidade after insert or update or delete on spotify_db.track
+for each statement execute procedure  spotify_db.album_acustico_popularidade();
